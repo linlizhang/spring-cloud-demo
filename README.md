6d1900f1a6e80ce108cef97e9d73087baedc1a2f
Zhang Linli <zhangmagle125@163.com>
2021-07-31 22:25:08 +0800

    enable security for Eureka cluster

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spring-cloud-demo
 This repository is to create a spring cloud demo including service discovery, load-balancing and gateway, etc.
+
+
--- services-discovery-eureka/src/main/java/com/zhangmagle/ServiceDiscoveryEurekaApplication.java
+++ services-discovery-eureka/src/main/java/com/zhangmagle/ServiceDiscoveryEurekaApplication.java
@@ -3,6 +3,9 @@ package com.zhangmagle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +13,17 @@ public class ServiceDiscoveryEurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceDiscoveryEurekaApplication.class, args);
+	}
+	
+	@EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().ignoringAntMatchers("/eureka/**");
+			super.configure(http);
+		}
+		
 	}
 
 }
